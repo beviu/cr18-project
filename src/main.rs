5e797616731c9e326523f5c098cb2bf59f41074b
beviu <contact@beviu.com>
2025-02-05 19:15:04 +0100

    Add fixed files to Rust version

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,28 @@ use std::{
     time::{Duration, Instant},
 };
 
+use clap::Parser;
+
+#[derive(clap::Parser)]
+struct Args {
+    /// Use fixed files instead of file descriptors.
+    #[clap(short, long)]
+    fixed_files: bool,
+}
+
 fn main() {
+    let args = Args::parse();
+
     let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
     let mut ring = io_uring::IoUring::new(8).unwrap();
+
+    ring.submitter()
+        .register_files(&[socket.as_raw_fd()])
+        .unwrap();
+
     let start = Instant::now();
     let mut datagram_count = 0;
     while start.elapsed() < Duration::from_secs(1) {
-        let fd = io_uring::types::Fd(socket.as_raw_fd());
         let buf = [0u8; 16];
 
         let addr = libc::sockaddr_in {
@@ -23,11 +38,20 @@ fn main() {
             sin_zero: [0; 8],
         };
 
-        let entry =
-            io_uring::opcode::Send::new(fd, &buf as *const _, u32::try_from(buf.len()).unwrap())
-                .dest_addr(&addr as *const libc::sockaddr_in as *const _)
-                .dest_addr_len(u32::try_from(mem::size_of_val(&addr)).unwrap())
-                .build();
+        let len = u32::try_from(buf.len()).unwrap();
+        let buf = &buf as *const u8;
+        let send = if args.fixed_files {
+            let fixed = io_uring::types::Fixed(0);
+            io_uring::opcode::Send::new(fixed, buf, len)
+        } else {
+            let fd = io_uring::types::Fd(socket.as_raw_fd());
+            io_uring::opcode::Send::new(fd, buf, len)
+        };
+
+        let entry = send
+            .dest_addr(&addr as *const libc::sockaddr_in as *const _)
+            .dest_addr_len(u32::try_from(mem::size_of_val(&addr)).unwrap())
+            .build();
 
         unsafe {
             ring.submission().push(&entry).unwrap();
