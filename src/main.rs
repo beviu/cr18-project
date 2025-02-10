0eab198e7a78a07d11581063a080ded14fdabbe2
beviu <contact@beviu.com>
2025-02-10 10:58:31 +0100

    Stop using atomics for the datagram counter
    In the hot path, do not use atomics for the datagram counter. The
    counter from all threads is still accumulated into an atomic at the end,
    but this is only done once.

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,140 @@ struct Args {
     threads: Option<NonZeroUsize>,
 }
 
+fn send_datagrams(
+    socket: &UdpSocket,
+    fixed_files: bool,
+    fixed_buffers: bool,
+    zero_copy: bool,
+    buf_ring: bool,
+) -> u64 {
+    let mut ring = io_uring::IoUring::new(8).unwrap();
+    let (submitter, mut submission, mut completion) = ring.split();
+
+    submitter.register_files(&[socket.as_raw_fd()]).unwrap();
+
+    let buf_ring_mmap = BufRingMmap::new(8).unwrap();
+    let mut buf_ring = BufRing::register(&submitter, 0, buf_ring_mmap).unwrap();
+
+    const BUF_SIZE: usize = 16;
+
+    let mut bufs = Vec::new();
+    let mut iovecs = Vec::new();
+
+    for i in 0..buf_ring.entry_count() {
+        let mut buf = Box::new([1u8; BUF_SIZE]);
+
+        bufs.push(buf.as_mut_ptr());
+
+        iovecs.push(libc::iovec {
+            iov_base: buf.as_mut_ptr() as *mut c_void,
+            iov_len: buf.len(),
+        });
+
+        // Note: this leaks the memory.
+        unsafe {
+            buf_ring.add_buffer(Box::into_raw(buf), i);
+        }
+    }
+
+    if fixed_buffers {
+        unsafe {
+            submitter.register_buffers(&iovecs).unwrap();
+        }
+    }
+
+    let start = Instant::now();
+
+    let addr = libc::sockaddr_in {
+        sin_family: u16::try_from(libc::AF_INET).unwrap(),
+        sin_port: 12000u16.to_be(),
+        sin_addr: libc::in_addr {
+            s_addr: libc::INADDR_LOOPBACK.to_be(),
+        },
+        sin_zero: [0; 8],
+    };
+
+    let mut in_flight = 0;
+    let mut datagram_count = 0;
+
+    loop {
+        let keep_sending = start.elapsed() < Duration::from_secs(1);
+        if keep_sending {
+            while !submission.is_full() {
+                let len = u32::try_from(BUF_SIZE).unwrap();
+
+                let entry = if zero_copy {
+                    let mut send = if fixed_files {
+                        let fixed = io_uring::types::Fixed(0);
+                        io_uring::opcode::SendZc::new(fixed, bufs[0], len)
+                    } else {
+                        let fd = io_uring::types::Fd(socket.as_raw_fd());
+                        io_uring::opcode::SendZc::new(fd, bufs[0], len)
+                    };
+
+                    if fixed_buffers {
+                        send = send.buf_index(Some(0));
+                    }
+
+                    send.dest_addr(&addr as *const libc::sockaddr_in as *const _)
+                        .dest_addr_len(u32::try_from(mem::size_of_val(&addr)).unwrap())
+                        .build()
+                } else {
+                    let send = if fixed_files {
+                        let fixed = io_uring::types::Fixed(0);
+                        io_uring::opcode::Send::new(fixed, bufs[0], len)
+                    } else {
+                        let fd = io_uring::types::Fd(socket.as_raw_fd());
+                        io_uring::opcode::Send::new(fd, bufs[0], len)
+                    };
+
+                    if fixed_buffers {
+                        panic!("fixed buffers is only supported when zero-copy is enabled");
+                    }
+
+                    send.dest_addr(&addr as *const libc::sockaddr_in as *const _)
+                        .dest_addr_len(u32::try_from(mem::size_of_val(&addr)).unwrap())
+                        .build()
+                };
+
+                unsafe {
+                    submission.push(&entry).unwrap();
+                }
+
+                in_flight += 1;
+            }
+
+            submission.sync();
+        } else if in_flight == 0 {
+            break;
+        }
+
+        if in_flight > 0 {
+            completion.sync();
+
+            for entry in &mut completion {
+                if io_uring::cqueue::notif(entry.flags()) {
+                    continue;
+                }
+                if entry.result() < 0 {
+                    eprintln!("sendto: {}", entry.result());
+                    return datagram_count;
+                }
+                datagram_count += 1;
+                in_flight -= 1;
+            }
+        }
+
+        if in_flight > 0 {
+            submitter.submit_and_wait(1).unwrap();
+        } else {
+            submitter.squeue_wait().unwrap();
+        }
+    }
+
+    datagram_count
+}
+
 fn main() {
     let args = Args::parse();
 
@@ -51,130 +185,16 @@ fn main() {
     thread::scope(|s| {
         for _ in 0..threads.get() {
             s.spawn(|| {
-                let mut ring = io_uring::IoUring::new(8).unwrap();
-                let (submitter, mut submission, mut completion) = ring.split();
-
-                submitter.register_files(&[socket.as_raw_fd()]).unwrap();
-
-                let buf_ring_mmap = BufRingMmap::new(8).unwrap();
-                let mut buf_ring = BufRing::register(&submitter, 0, buf_ring_mmap).unwrap();
-
-                const BUF_SIZE: usize = 16;
-
-                let mut bufs = Vec::new();
-                let mut iovecs = Vec::new();
-
-                for i in 0..buf_ring.entry_count() {
-                    let mut buf = Box::new([1u8; BUF_SIZE]);
-
-                    bufs.push(buf.as_mut_ptr());
-
-                    iovecs.push(libc::iovec {
-                        iov_base: buf.as_mut_ptr() as *mut c_void,
-                        iov_len: buf.len(),
-                    });
-
-                    // Note: this leaks the memory.
-                    unsafe {
-                        buf_ring.add_buffer(Box::into_raw(buf), i);
-                    }
-                }
-
-                if args.fixed_buffers {
-                    unsafe {
-                        submitter.register_buffers(&iovecs).unwrap();
-                    }
-                }
-
-                let start = Instant::now();
-
-                let addr = libc::sockaddr_in {
-                    sin_family: u16::try_from(libc::AF_INET).unwrap(),
-                    sin_port: 12000u16.to_be(),
-                    sin_addr: libc::in_addr {
-                        s_addr: libc::INADDR_LOOPBACK.to_be(),
-                    },
-                    sin_zero: [0; 8],
-                };
-
-                let mut in_flight = 0;
-
-                loop {
-                    let keep_sending = start.elapsed() < Duration::from_secs(1);
-                    if keep_sending {
-                        while !submission.is_full() {
-                            let len = u32::try_from(BUF_SIZE).unwrap();
-
-                            let entry = if args.zero_copy {
-                                let mut send = if args.fixed_files {
-                                    let fixed = io_uring::types::Fixed(0);
-                                    io_uring::opcode::SendZc::new(fixed, bufs[0], len)
-                                } else {
-                                    let fd = io_uring::types::Fd(socket.as_raw_fd());
-                                    io_uring::opcode::SendZc::new(fd, bufs[0], len)
-                                };
-
-                                if args.fixed_buffers {
-                                    send = send.buf_index(Some(0));
-                                }
-
-                                send.dest_addr(&addr as *const libc::sockaddr_in as *const _)
-                                    .dest_addr_len(u32::try_from(mem::size_of_val(&addr)).unwrap())
-                                    .build()
-                            } else {
-                                let send = if args.fixed_files {
-                                    let fixed = io_uring::types::Fixed(0);
-                                    io_uring::opcode::Send::new(fixed, bufs[0], len)
-                                } else {
-                                    let fd = io_uring::types::Fd(socket.as_raw_fd());
-                                    io_uring::opcode::Send::new(fd, bufs[0], len)
-                                };
-
-                                if args.fixed_buffers {
-                                    panic!(
-                                        "fixed buffers is only supported when zero-copy is enabled"
-                                    );
-                                }
-
-                                send.dest_addr(&addr as *const libc::sockaddr_in as *const _)
-                                    .dest_addr_len(u32::try_from(mem::size_of_val(&addr)).unwrap())
-                                    .build()
-                            };
-
-                            unsafe {
-                                submission.push(&entry).unwrap();
-                            }
-
-                            in_flight += 1;
-                        }
-
-                        submission.sync();
-                    } else if in_flight == 0 {
-                        break;
-                    }
-
-                    if in_flight > 0 {
-                        completion.sync();
-
-                        for entry in &mut completion {
-                            if io_uring::cqueue::notif(entry.flags()) {
-                                continue;
-                            }
-                            if entry.result() < 0 {
-                                eprintln!("sendto: {}", entry.result());
-                                return;
-                            }
-                            datagram_count.fetch_add(1, Ordering::Relaxed);
-                            in_flight -= 1;
-                        }
-                    }
-
-                    if in_flight > 0 {
-                        submitter.submit_and_wait(1).unwrap();
-                    } else {
-                        submitter.squeue_wait().unwrap();
-                    }
-                }
+                datagram_count.fetch_add(
+                    send_datagrams(
+                        &socket,
+                        args.fixed_files,
+                        args.fixed_buffers,
+                        args.zero_copy,
+                        args.buf_ring,
+                    ),
+                    Ordering::Relaxed,
+                );
             });
         }
     });
