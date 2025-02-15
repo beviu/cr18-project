ca54d4c0803e1f297153a93e4964c770e517cf9b
beviu <contact@beviu.com>
2025-02-15 12:30:09 +0100

    Move reception code to src/server.rs

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ use io_uring::{
 };
 use io_uring_buf_ring::IoUringBufRing;
 
+mod server;
 mod zcrx;
 
 #[derive(clap::Parser)]
@@ -243,179 +244,6 @@ fn send_datagrams(
     datagram_count
 }
 
-fn receive_datagrams(
-    socket: &UdpSocket,
-    fixed_files: bool,
-    fixed_buffers: bool,
-    single_issuer: bool,
-    coop_taskrun: bool,
-    use_buf_ring: bool,
-    stop: &AtomicU32,
-) -> u64 {
-    let mut builder: io_uring::Builder<squeue::Entry, cqueue::Entry> = IoUring::builder();
-
-    if single_issuer {
-        builder.setup_single_issuer();
-    }
-
-    if coop_taskrun {
-        builder.setup_coop_taskrun();
-    }
-
-    let mut ring = builder
-        .build(8)
-        .expect("failed to create thread io_uring instance");
-
-    if fixed_files {
-        ring.submitter()
-            .register_files(&[socket.as_raw_fd()])
-            .unwrap();
-    }
-
-    let buf_ring = IoUringBufRing::new(&ring, 8, 0, 16).expect("failed to create buf_ring");
-    let mut buf_ring_available_count = 8;
-
-    const BUF_SIZE: usize = 16;
-
-    let mut iovecs = Vec::new();
-    let mut bufs = Vec::new();
-    let mut available_buf_indices = Vec::new();
-
-    for buf_index in 0..8 {
-        let mut buf = Box::new([1u8; BUF_SIZE]);
-        iovecs.push(libc::iovec {
-            iov_base: buf.as_mut_ptr() as *mut c_void,
-            iov_len: buf.len(),
-        });
-        bufs.push(buf);
-        available_buf_indices.push(buf_index);
-    }
-
-    if fixed_buffers {
-        unsafe {
-            ring.submitter().register_buffers(&iovecs).unwrap();
-        }
-    }
-
-    const USER_DATA_STOP: u64 = 0;
-    const USER_DATA_RECV_FIRST: u64 = 1;
-
-    {
-        const FUTEX2_SIZE_U32: u32 = 0x2;
-        let wait = io_uring::opcode::FutexWait::new(
-            stop.as_ptr() as *const _,
-            0,
-            libc::FUTEX_BITSET_MATCH_ANY as c_uint as u64,
-            FUTEX2_SIZE_U32 | libc::FUTEX_PRIVATE_FLAG as c_uint,
-        )
-        .build()
-        .user_data(USER_DATA_STOP);
-
-        let mut submission = ring.submission();
-        unsafe {
-            submission.push(&wait).unwrap();
-        }
-    }
-
-    let mut datagram_count = 0;
-
-    'main_loop: while stop.load(Ordering::Relaxed) == 0 {
-        if !available_buf_indices.is_empty() {
-            let mut submission = ring.submission();
-            while !submission.is_full() && !available_buf_indices.is_empty() {
-                let len = u32::try_from(BUF_SIZE).unwrap();
-                let entry = if use_buf_ring {
-                    if buf_ring_available_count == 0 {
-                        break;
-                    }
-                    buf_ring_available_count -= 1;
-
-                    let recv = if fixed_files {
-                        let fixed = io_uring::types::Fixed(0);
-                        io_uring::opcode::Recv::new(fixed, ptr::null_mut(), len)
-                    } else {
-                        let fd = io_uring::types::Fd(socket.as_raw_fd());
-                        io_uring::opcode::Recv::new(fd, ptr::null_mut(), len)
-                    };
-
-                    recv.buf_group(buf_ring.buffer_group())
-                        .build()
-                        .user_data(USER_DATA_RECV_FIRST)
-                        .flags(squeue::Flags::BUFFER_SELECT)
-                } else {
-                    let Some(buf_index) = available_buf_indices.pop() else {
-                        break;
-                    };
-
-                    let recv = if fixed_files {
-                        let fixed = io_uring::types::Fixed(0);
-                        io_uring::opcode::Recv::new(fixed, bufs[buf_index].as_mut_ptr(), len)
-                    } else {
-                        let fd = io_uring::types::Fd(socket.as_raw_fd());
-                        io_uring::opcode::Recv::new(fd, bufs[buf_index].as_mut_ptr(), len)
-                    };
-
-                    recv.build()
-                        .user_data(USER_DATA_RECV_FIRST + u64::try_from(buf_index).unwrap())
-                };
-                unsafe {
-                    submission.push(&entry).unwrap();
-                }
-            }
-        }
-
-        for entry in ring.completion() {
-            match entry.user_data() {
-                USER_DATA_STOP => {
-                    if entry.result() < 0 {
-                        eprintln!("futex_wait: {}", entry.result());
-                        break 'main_loop;
-                    }
-                }
-                user_data => {
-                    if use_buf_ring {
-                        if user_data != USER_DATA_RECV_FIRST {
-                            panic!("unexpected user data {user_data} in CQE");
-                        }
-                        let buf_id = cqueue::buffer_select(entry.flags())
-                            .expect("missing buffer ID in recv CQE");
-                        match usize::try_from(entry.result()) {
-                            Ok(available_len) => {
-                                datagram_count += 1;
-                                mem::drop(unsafe { buf_ring.get_buf(buf_id, available_len) });
-                                buf_ring_available_count += 1;
-                            }
-                            Err(_) => eprintln!("recv: {}", entry.result()),
-                        }
-                    } else {
-                        if let Some(buf_index) = user_data.checked_sub(USER_DATA_RECV_FIRST) {
-                            if entry.result() < 0 {
-                                eprintln!("recv: {}", entry.result());
-                            } else {
-                                datagram_count += 1;
-                            }
-                            available_buf_indices.push(usize::try_from(buf_index).unwrap());
-                        } else {
-                            panic!("unexpected user data {user_data} in CQE");
-                        }
-                    }
-                }
-            }
-        }
-
-        ring.submitter().submit_and_wait(1).unwrap();
-    }
-
-    ring.submitter()
-        .register_sync_cancel(None, CancelBuilder::any())
-        .expect("failed to cancel pending requests");
-
-    if let Err(err) = unsafe { buf_ring.release(&ring) } {
-        eprintln!("failed to release buf_ring: {err}");
-    }
-
-    datagram_count
-}
 
 fn mask_sigint() -> io::Result<()> {
     let mut mask: MaybeUninit<libc::sigset_t> = MaybeUninit::uninit();
@@ -518,7 +346,7 @@ fn main() {
         for _ in 0..thread_count.get() {
             threads.push(s.spawn(|| {
                 if args.server {
-                    receive_datagrams(
+                    server::receive_datagrams(
                         &socket,
                         args.fixed_files,
                         args.fixed_buffers,
