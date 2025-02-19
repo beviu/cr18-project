3d1e22b71c8a90b7cb37e0662a52cf45064c06da
beviu <contact@beviu.com>
2025-02-19 14:56:29 +0100

    Replace mio by direct calls to libc

--- server-epoll/src/main.rs
+++ server-epoll/src/main.rs
@@ -1,7 +1,12 @@
-use std::io::{self, Read};
+use std::{
+    io,
+    mem::MaybeUninit,
+    net::TcpListener,
+    os::fd::{AsFd, AsRawFd, BorrowedFd, FromRawFd, IntoRawFd, OwnedFd, RawFd},
+    ptr,
+};
 
 use clap::Parser;
-use mio::{net::TcpListener, Events, Interest, Poll, Token};
 
 #[derive(clap::Parser)]
 struct Args {
@@ -9,58 +14,138 @@ struct Args {
     bind: String,
 }
 
+fn epoll_create1(flags: i32) -> io::Result<OwnedFd> {
+    let ret = unsafe { libc::epoll_create1(flags) };
+    if ret == -1 {
+        return Err(io::Error::last_os_error());
+    }
+    Ok(unsafe { OwnedFd::from_raw_fd(ret) })
+}
+
+fn epoll_ctl_add(
+    epoll_fd: &impl AsRawFd,
+    fd: &impl AsRawFd,
+    event: &libc::epoll_event,
+) -> io::Result<()> {
+    let ret = unsafe {
+        libc::epoll_ctl(
+            epoll_fd.as_raw_fd(),
+            libc::EPOLL_CTL_ADD,
+            fd.as_raw_fd(),
+            event as *const _ as *mut _,
+        )
+    };
+    if ret == -1 {
+        return Err(io::Error::last_os_error());
+    }
+    Ok(())
+}
+
+fn epoll_ctl_del(epoll_fd: &impl AsRawFd, fd: &impl AsRawFd) -> io::Result<()> {
+    let ret = unsafe {
+        libc::epoll_ctl(
+            epoll_fd.as_raw_fd(),
+            libc::EPOLL_CTL_DEL,
+            fd.as_raw_fd(),
+            ptr::null_mut(),
+        )
+    };
+    if ret == -1 {
+        return Err(io::Error::last_os_error());
+    }
+    Ok(())
+}
+
+fn epoll_wait(
+    epoll_fd: &impl AsRawFd,
+    events: *mut libc::epoll_event,
+    capacity: i32,
+    timeout: i32,
+) -> io::Result<i32> {
+    let ret = unsafe { libc::epoll_wait(epoll_fd.as_raw_fd(), events, capacity, timeout) };
+    if ret == -1 {
+        return Err(io::Error::last_os_error());
+    }
+    Ok(ret)
+}
+
+fn read(fd: &impl AsRawFd, buf: &mut [MaybeUninit<u8>]) -> io::Result<isize> {
+    let ret = unsafe { libc::read(fd.as_raw_fd(), buf.as_mut_ptr().cast(), buf.len()) };
+    if ret == -1 {
+        return Err(io::Error::last_os_error());
+    }
+    Ok(ret)
+}
+
+fn handle_event(event: &libc::epoll_event, epoll_fd: BorrowedFd, socket: &TcpListener) {
+    // The user data is `u64::MAX` for the server socket and the client file descriptor for client
+    // sockets.
+    if event.u64 == u64::MAX {
+        loop {
+            let (client, _addr) = match socket.accept() {
+                Ok(ret) => ret,
+                Err(err) if err.kind() == io::ErrorKind::WouldBlock => break,
+                Err(err) => panic!("failed to accept: {err}"),
+            };
+
+            epoll_ctl_add(
+                &epoll_fd,
+                &client,
+                &libc::epoll_event {
+                    events: libc::EPOLLIN as u32,
+                    u64: client.as_raw_fd() as u64,
+                },
+            )
+            .unwrap();
+
+            let _fd = client.into_raw_fd();
+        }
+    } else {
+        let fd: RawFd = event.u64 as i32;
+
+        loop {
+            let mut buf = [MaybeUninit::uninit(); 4096];
+            let n = match read(&fd, &mut buf) {
+                Ok(ret) => ret,
+                Err(err) if err.kind() == io::ErrorKind::WouldBlock => break,
+                Err(err) => panic!("failed to read: {err}"),
+            };
+            if n == 0 {
+                epoll_ctl_del(&epoll_fd, &fd).unwrap();
+                drop(unsafe { OwnedFd::from_raw_fd(fd) });
+            }
+        }
+    }
+}
+
 fn main() {
     let args = Args::parse();
 
-    let mut socket = TcpListener::bind(args.bind.parse().unwrap()).unwrap();
+    let socket = TcpListener::bind(&args.bind).unwrap();
+    socket.set_nonblocking(true).unwrap();
 
-    let mut poll = Poll::new().unwrap();
-    let mut events = Events::with_capacity(128);
+    let epoll_fd = epoll_create1(libc::EPOLL_CLOEXEC).unwrap();
 
-    const SERVER: Token = Token(usize::MAX);
-    poll.registry()
-        .register(&mut socket, SERVER, Interest::READABLE).unwrap();
+    epoll_ctl_add(
+        &epoll_fd,
+        &socket,
+        &libc::epoll_event {
+            events: libc::EPOLLIN as u32,
+            u64: u64::MAX,
+        },
+    )
+    .unwrap();
 
-    let mut clients = Vec::new();
+    let mut events = Vec::with_capacity(1024);
 
     loop {
-        poll.poll(&mut events, None).unwrap();
+        let n = epoll_wait(&epoll_fd, events.as_mut_ptr(), events.capacity() as i32, 0).unwrap();
+        unsafe {
+            events.set_len(n as usize);
+        }
 
-        for event in events.iter() {
-            if event.token() == SERVER {
-                loop {
-                    let (mut client, _addr) = match socket.accept() {
-                        Ok(ret) => ret,
-                        Err(err) if err.kind() == io::ErrorKind::WouldBlock => break,
-                        Err(err) => panic!("failed to accept: {err}"),
-                    };
-
-                    let token = Token(clients.len());
-                    poll.registry()
-                        .register(&mut client, token, Interest::READABLE).unwrap();
-                
-                    clients.push(Some(client));
-                }
-            } else {
-                let remove = if let Some(client) = &mut clients[event.token().0] {
-                    loop {
-                        let mut buf = [0; 256];
-                        let n = match client.read(&mut buf) {
-                            Ok(ret) => ret,
-                            Err(err) if err.kind() == io::ErrorKind::WouldBlock => break false,
-                            Err(err) => panic!("failed to read: {err}"),
-                        };
-                        if n == 0 {
-                            break true;
-                        }
-                    }
-                } else {
-                    false
-                };
-                if remove {
-                    clients[event.token().0] = None;
-                }
-            }
+        for event in &events {
+            handle_event(&event, epoll_fd.as_fd(), &socket);
         }
     }
 }
